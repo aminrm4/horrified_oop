34850f3e46dad4a674735fe9fb4d04051d0bdc7c
Amin rahimi <aminmehr284@gmail.com>
2025-06-10 17:12:19 +0330

    add::refactor location and mosnter

--- include/location.hpp
+++ include/location.hpp
@@ -2,24 +2,27 @@
 #define LOCATION_HPP
 #include <bits/stdc++.h>
 #include "villager.hpp"
+#include"item.hpp"
 class location
 {
-protected:
+private:
     int loc_relation;
     bool coffin_exist;
     std::vector<villager *> villager_list;
+    std::vector<item*> item_list;
 
 public:
     void set_coffin_exist(bool);
     void set_villager(villager *);
     void set_loc_relation(int);
+     void set_item_list(item*);
     int get_loc_relation() const;
     bool get_coffin_exist() const;
      std ::vector<villager *> &get_villager_list();
-
-    location(int, bool, villager *);
+    std::vector<item*> & get_item_list();
+    location(int, bool, villager *,item * );
     location() = delete;
-    virtual ~location();
+    ~location();
 };
 
 #endif
--- src/overstock.cpp
+++ src/overstock.cpp
@@ -1,15 +1,22 @@
 #include "overstock.hpp"
-#include"programm.hpp"
+#include "programm.hpp"
 #include <iostream>
-#include<iterator>
+#include <iterator>
 using namespace std;
-void overstock::play(hero * he, std::vector<hero *>her_list, monster * mon, location * loc, std::vector<item *> item_list, std::vector<monster *> mon_list, std::vector<std::vector<int>> map)
+void overstock::play(hero *he, std::vector<hero *> her_list, monster *mon, location *loc, std::vector<item *> item_list, std::vector<monster *> mon_list, std::vector<std::vector<int>> map)
 
 {
      programm help_object;
-     auto it=help_object.random_generator(1,35);
-     auto item1= item_list[it.first]->get_loc();
-     auto item2=item_list[it.second]->get_loc();          //monster are players? 
-     //auto child_saver=dynamic_cast<i>(loc);  
-
+          set<int>::iterator accesser;
+          set<int>temp;
+     temp = help_object.random_generator(1, 35, 2);
+     accesser=temp.begin();
+     auto item1 = item_list[*accesser];
+     accesser++;
+     auto item2 = item_list[*accesser]; // monster are players?
+     loc->set_item_list(item1);
+     loc->set_item_list(item2);
+     item_list.erase(item_list.begin()+*accesser);
+     accesser--;
+     item_list.erase(item_list.begin()+*accesser);
 }
