be01a14fbaa60144dbbca4ff76916f6859ccf16a
Amin rahimi <aminmehr284@gmail.com>
2025-07-01 19:34:30 +0330

    ui more ability

--- include/programm.hpp
+++ include/programm.hpp
@@ -26,7 +26,10 @@ struct LocationInfo
   std::string villagers;
   std::string hero;
 };
+struct heroinfo {
+  std::string name;
 
+};
 class programm
 {
 public:
@@ -49,13 +52,17 @@ public:
   bool check_terro_night();
   void run();
   programm();
-  void terminal_handler(LocationInfo &info, string &, string &);
+  void terminal_handler(LocationInfo &info, heroinfo &inf, string &, string &);
   string show_all_item(const vector<item *> &show, LocationInfo &);
   string show_all_villager(const vector<villager *> &, LocationInfo &);
   string show_all_hero(const vector<hero *> &, LocationInfo &);
   string show_all_mosnter(const vector<monster *> &, LocationInfo &);
-  template <typename T>
-  string show_hero_deatail(T);
+   string show_hero_item(const vector<item *> &show, heroinfo &);
+  string show_hero_perk(const vector<perk*> &, heroinfo &);
+  string show_hero_action(const vector<hero *> &, heroinfo &);
+  string show_hero_name(const vector<hero*> &, heroinfo &);
   void clearScreen();
   ~programm();
+  template <typename T>
+string show_hero_deatail(T vec);
 };
