b3d7cf2c9aaeca4fbb3fe142a68ba508c1cbdc27
Ali kermani <alikermanihabibabadi14@gmail.com>
2025-08-03 23:29:08 +0330

    the loading problem for item assets fixed

--- src/On_the_move.cpp
+++ src/On_the_move.cpp
@@ -3,7 +3,6 @@
 void remove_villager(programm &, villager *);
 void On_the_move::event(std::vector<std::vector<int>> &map, std::vector<location *> &loc, programm &a)
 {
-    cout << "on the move monster card is done" << endl;
     vector<int> route;
     a.next_frenzy();
     for (int i = 0; i < a.list_of_location.size(); i++)
@@ -13,16 +12,25 @@ void On_the_move::event(std::vector<std::vector<int>> &map, std::vector<location
             route = (a.bfs(vill->get_currnet_location()->get_loc_relation(), vill->get_safe_location()->get_loc_relation()));
             if (route.empty())
             {
-                return;
+                continue;
             }
             else
             {
+                cout << vill->get_name() << endl;
+                cout << a.list_of_location.at(i)->get_loc_relation() << endl;
                 remove_villager(a, vill);
                 a.list_of_location[route[1]]->set_villager(vill);
                 vill->set_current_location(a.list_of_location[route[1]]);
+                cout << a.list_of_location.at(i)->get_loc_relation() << endl;
+                if (vill->get_currnet_location() == vill->get_safe_location())
+                {
+                    remove_villager(a, vill);
+                    delete vill;
+                }
             }
         }
     }
+    cout << "on the move monster card is done" << endl;
 }
 void On_the_move::monster_strike(programm &bug, vector<monster *> &monsters)
 {
@@ -45,5 +53,5 @@ On_the_move::On_the_move(int dic, int my_item, int mover)
     dice_play = dic;
     item_count = my_item;
     move_left = mover;
-    this->name_of_card="OnTheMove";
+    this->name_of_card = "OnTheMove";
 }
